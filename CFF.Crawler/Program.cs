d5447eed6d44279aa5d990695f3b6bc8e1d24773
Reborn <vubao1690@gmail.com>
2019-12-08 21:36:14 +0700

    Crawler data

--- CFF.Crawler/Program.cs
+++ CFF.Crawler/Program.cs
@@ -13,19 +13,24 @@ namespace CFF.Crawler
 {
     class Program
     {
-        private const string filePath = @"/home/reborn/Downloads/Data/CrawlerData";
+        private const string filePathTravel = @"/Users/reborn/SourceCode/tripbffcrawler/Data/TravelData";
+        private const string filePathRestaurant = @"/Users/reborn/SourceCode/tripbffcrawler/Data/RestaurantData";
+        private const string filePathEdu = @"/Users/reborn/SourceCode/tripbffcrawler/Data/EducationData";
+        private const string filePathEntertain = @"/Users/reborn/SourceCode/tripbffcrawler/Data/EntertainData";
 
         static void Main(string[] args)
         {
-            var isSuccess = CrawlerRestaurantData();
-            //var restaurants = GetDealRestaurantsFromFile();
+            //CrawlerTravelData();
+            CrawlerRestaurantData();
+            //CrawlerEntertainData();
+            //CrawlerEducationData();
         }
 
-        private static object GetDealRestaurantsFromFile()
-        {
-            var restaurants = ReadFromJsonFile<List<RestaurantModel>>(filePath);
-            return restaurants;
-        }
+        //private static object GetDealRestaurantsFromFile()
+        //{
+        //    var restaurants = ReadFromJsonFile<List<RestaurantModel>>(filePath);
+        //    return restaurants;
+        //}
 
         private static bool CrawlerTravelData()
         {
@@ -141,10 +146,16 @@ namespace CFF.Crawler
                         locations.Add(location);
                     }
 
-                    Thread.Sleep(15000);
+                    Thread.Sleep(500);
                 }
 
-                WriteToJsonFile(filePath, locations);
+                WriteToJsonFile(filePathTravel, locations.Select(lo => new {
+                    lo.Title,
+                    lo.Address,
+                    lo.Long,
+                    lo.Lat
+                }).ToList());
+
                 driver.Quit();
             }
 
@@ -154,7 +165,7 @@ namespace CFF.Crawler
         private static bool CrawlerRestaurantData()
         {
             //Load html content 
-            using (IWebDriver driver = new ChromeDriver("/home/reborn/Downloads/chromedriver_linux64"))
+            using (IWebDriver driver = new ChromeDriver("/usr/local/bin"))
             {
                 var rootUrl = "https://www.foody.vn/";
                 driver.Navigate().GoToUrl(rootUrl);
@@ -267,7 +278,7 @@ namespace CFF.Crawler
                         locations.Add(location);
                     }
 
-                    Thread.Sleep(1000);
+                    Thread.Sleep(500);
                 }
                 
                 // get long, lat of locations
@@ -293,20 +304,350 @@ namespace CFF.Crawler
                     {
                     }
 
-                    Thread.Sleep(3000);
+                    Thread.Sleep(500);
                 }
 
-                WriteToJsonFile(filePath, locations.Select(lo => new { 
-                    lo.Title,
-                    lo.Address,
-                    lo.Long,
-                    lo.Lat
-                }).ToList());
+                var result = new
+                {
+                    Locations = locations.Select(lo => new {
+                        lo.Title,
+                        lo.Address,
+                        lo.Long,
+                        lo.Lat
+                    }).ToList()
+                };
+
+                WriteToJsonFile(filePathRestaurant, result);
                 driver.Quit();
             }
 
             return true;
         }
+
+        private static bool CrawlerEntertainData()
+        {
+            //Load html content 
+            using (IWebDriver driver = new ChromeDriver("/usr/local/bin"))
+            {
+                var rootUrl = "https://www.foody.vn/";
+                driver.Navigate().GoToUrl(rootUrl);
+                WaitForReady(driver, TimeSpan.FromSeconds(30));
+
+                var citiesDropdownElement = driver.FindElement(By.ClassName("rn-nav-name"));
+                citiesDropdownElement.Click();
+
+                IWebElement flpCountriesElement = null;
+
+                while (true)
+                {
+                    try
+                    {
+                        flpCountriesElement = driver.FindElement(By.XPath("//*[@id=\"popupLocation\"]/ul/li/ul"));
+
+                        if (flpCountriesElement != null)
+                            break;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                var citiesParentElements = flpCountriesElement.FindElements(By.TagName("li"));
+                var cityUrls = new List<string>();
+
+                foreach (var citiesParentElement in citiesParentElements)
+                {
+                    var cityParentElements = citiesParentElement.FindElements(By.TagName("li"));
+
+                    foreach (var cityParentElement in cityParentElements)
+                    {
+                        var linkElement = cityParentElement.FindElement(By.TagName("a"));
+                        var cityUrl = linkElement.GetAttribute("href") + "/entertain";
+                        cityUrls.Add(cityUrl);
+                    }
+                }
+
+                var locations = new List<LocationModel>();
+
+                foreach (var cityUrl in cityUrls)
+                {
+                    driver.Navigate().GoToUrl(cityUrl);
+                    var locationContainerElement = driver.FindElement(By.ClassName("content-container"));
+
+                    // handle load more
+                    while (true)
+                    {
+                        try
+                        {
+                            var loadMoreContainerElement = locationContainerElement.FindElement(By.ClassName("pn-loadmore"));
+
+                            if (loadMoreContainerElement != null)
+                            {
+                                var indexLoadMore = 0;
+
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        var loadMoreElement = loadMoreContainerElement.FindElement(By.ClassName("fd-btn-more"));
+
+                                        if (loadMoreElement != null)
+                                        {
+                                            loadMoreElement.Click();
+                                            break;
+                                        }
+                                    }
+                                    catch (Exception)
+                                    {
+                                    }
+
+                                    indexLoadMore++;
+
+                                    if (indexLoadMore == 100)
+                                        break;
+                                }
+                            }
+                            else
+                                break;
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    }
+
+                    WaitForReady(driver, TimeSpan.FromSeconds(30));
+
+                    locationContainerElement = driver.FindElement(By.ClassName("content-container"));
+                    var locationElements = locationContainerElement.FindElements(By.ClassName("content-item"));
+
+                    foreach (var locationElement in locationElements)
+                    {
+                        var locationItemContent = locationElement.FindElement(By.ClassName("items-content"));
+                        var titleElement = locationItemContent.FindElement(By.ClassName("title"));
+                        var urlElement = titleElement.FindElement(By.TagName("a"));
+                        var addressElement = locationItemContent.FindElement(By.ClassName("desc"));
+
+                        var title = urlElement.Text;
+                        var locationUrl = urlElement.GetAttribute("href");
+                        var address = addressElement.Text;
+                        var location = new LocationModel
+                        {
+                            Title = title,
+                            Address = address,
+                            Url = locationUrl
+                        };
+                        locations.Add(location);
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                // get long, lat of locations
+                foreach (var location in locations)
+                {
+                    try
+                    {
+                        driver.Navigate().GoToUrl(location.Url);
+                        var linkMapElements = driver.FindElements(By.ClassName("linkmap"));
+                        var linkMapElement = linkMapElements[2];
+
+                        var imgElement = linkMapElement.FindElement(By.TagName("img"));
+                        var src = imgElement.GetAttribute("src");
+                        var srcItems = src.Split('_');
+
+                        var longValue = srcItems[2].Split('.')[0].Replace("-", ".");
+                        var latValue = srcItems[1].Replace("-", ".");
+
+                        location.Long = longValue;
+                        location.Lat = latValue;
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                var result = new
+                {
+                    Locations = locations.Select(lo => new {
+                        lo.Title,
+                        lo.Address,
+                        lo.Long,
+                        lo.Lat
+                    }).ToList()
+                };
+
+                WriteToJsonFile(filePathEntertain, result);
+                driver.Quit();
+            }
+
+            return true;
+        }
+
+        private static bool CrawlerEducationData()
+        {
+            //Load html content 
+            using (IWebDriver driver = new ChromeDriver("/usr/local/bin"))
+            {
+                var rootUrl = "https://www.foody.vn/";
+                driver.Navigate().GoToUrl(rootUrl);
+                WaitForReady(driver, TimeSpan.FromSeconds(30));
+
+                var citiesDropdownElement = driver.FindElement(By.ClassName("rn-nav-name"));
+                citiesDropdownElement.Click();
+
+                IWebElement flpCountriesElement = null;
+
+                while (true)
+                {
+                    try
+                    {
+                        flpCountriesElement = driver.FindElement(By.XPath("//*[@id=\"popupLocation\"]/ul/li/ul"));
+
+                        if (flpCountriesElement != null)
+                            break;
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+
+                var citiesParentElements = flpCountriesElement.FindElements(By.TagName("li"));
+                var cityUrls = new List<string>();
+
+                foreach (var citiesParentElement in citiesParentElements)
+                {
+                    var cityParentElements = citiesParentElement.FindElements(By.TagName("li"));
+
+                    foreach (var cityParentElement in cityParentElements)
+                    {
+                        var linkElement = cityParentElement.FindElement(By.TagName("a"));
+                        var cityUrl = linkElement.GetAttribute("href") + "/edu";
+                        cityUrls.Add(cityUrl);
+                    }
+                }
+
+                var locations = new List<LocationModel>();
+
+                foreach (var cityUrl in cityUrls)
+                {
+                    driver.Navigate().GoToUrl(cityUrl);
+                    var locationContainerElement = driver.FindElement(By.ClassName("content-container"));
+
+                    // handle load more
+                    while (true)
+                    {
+                        try
+                        {
+                            var loadMoreContainerElement = locationContainerElement.FindElement(By.ClassName("pn-loadmore"));
+
+                            if (loadMoreContainerElement != null)
+                            {
+                                var indexLoadMore = 0;
+
+                                while (true)
+                                {
+                                    try
+                                    {
+                                        var loadMoreElement = loadMoreContainerElement.FindElement(By.ClassName("fd-btn-more"));
+
+                                        if (loadMoreElement != null)
+                                        {
+                                            loadMoreElement.Click();
+                                            break;
+                                        }
+                                    }
+                                    catch (Exception)
+                                    {
+                                    }
+
+                                    indexLoadMore++;
+
+                                    if (indexLoadMore == 100)
+                                        break;
+                                }
+                            }
+                            else
+                                break;
+                        }
+                        catch (Exception)
+                        {
+                            break;
+                        }
+                    }
+
+                    WaitForReady(driver, TimeSpan.FromSeconds(30));
+
+                    locationContainerElement = driver.FindElement(By.ClassName("content-container"));
+                    var locationElements = locationContainerElement.FindElements(By.ClassName("content-item"));
+
+                    foreach (var locationElement in locationElements)
+                    {
+                        var locationItemContent = locationElement.FindElement(By.ClassName("items-content"));
+                        var titleElement = locationItemContent.FindElement(By.ClassName("title"));
+                        var urlElement = titleElement.FindElement(By.TagName("a"));
+                        var addressElement = locationItemContent.FindElement(By.ClassName("desc"));
+
+                        var title = urlElement.Text;
+                        var locationUrl = urlElement.GetAttribute("href");
+                        var address = addressElement.Text;
+                        var location = new LocationModel
+                        {
+                            Title = title,
+                            Address = address,
+                            Url = locationUrl
+                        };
+                        locations.Add(location);
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                // get long, lat of locations
+                foreach (var location in locations)
+                {
+                    try
+                    {
+                        driver.Navigate().GoToUrl(location.Url);
+                        var linkMapElements = driver.FindElements(By.ClassName("linkmap"));
+                        var linkMapElement = linkMapElements[2];
+
+                        var imgElement = linkMapElement.FindElement(By.TagName("img"));
+                        var src = imgElement.GetAttribute("src");
+                        var srcItems = src.Split('_');
+
+                        var longValue = srcItems[2].Split('.')[0].Replace("-", ".");
+                        var latValue = srcItems[1].Replace("-", ".");
+
+                        location.Long = longValue;
+                        location.Lat = latValue;
+                    }
+                    catch (Exception)
+                    {
+                    }
+
+                    Thread.Sleep(500);
+                }
+
+                var result = new
+                {
+                    Locations = locations.Select(lo => new {
+                        lo.Title,
+                        lo.Address,
+                        lo.Long,
+                        lo.Lat
+                    }).ToList()
+                };
+
+                WriteToJsonFile(filePathEdu, result);
+                driver.Quit();
+            }
+
+            return true;
+        }
+
 
         private static void WaitForReady(IWebDriver webDriver, TimeSpan timeout)
         {
@@ -331,7 +672,7 @@ namespace CFF.Crawler
             wait.Until(wd => js.ExecuteScript("return document.readyState").ToString() == "complete");
         }
 
-        public static void WriteToJsonFile<T>(string filePath, T objectToWrite, bool append = false) where T : new()
+        public static void WriteToJsonFile(string filePath, object objectToWrite, bool append = false)
         {
             TextWriter writer = null;
             try
